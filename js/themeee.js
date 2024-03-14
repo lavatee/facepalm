60797f7816cefcee5dcb33b9fd49e96d28e6796a
lavatee <161846965+lavatee@users.noreply.github.com>
2024-03-14 19:54:53 +0700

    Update themeee.js

--- js/themeee.js
+++ js/themeee.js
@@ -1,12 +1,12 @@
 let st = document.getElementById('st');
 function theme() {
   let th = document.getElementById('theme');
-  if (st.getAttribute("href") == "css/style.css") {
-    st.href = "css/masterlight.css";
+  if (st.getAttribute("href") == "css/masterlight.css") {
+    st.href = "css/style.css";
     th.textContent = "ТЕМНАЯ ТЕМА";
   }
   else {
-    st.href = "css/style.css";
+    st.href = "css/masterlight.css";
     th.textContent = "СВЕТЛАЯ ТЕМА";
   };
   localStorage.removeItem('theme');
