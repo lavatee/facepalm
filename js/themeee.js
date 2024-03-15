310a9dae7f8869e7e2159273e69a7baf271cc8d1
lavatee <161846965+lavatee@users.noreply.github.com>
2024-03-15 18:31:35 +0700

    Update themeee.js

--- js/themeee.js
+++ js/themeee.js
@@ -1,15 +1,18 @@
 let st = document.getElementById('st');
+let th = document.getElementById('theme');
 function theme() {
-  let th = document.getElementById('theme');
-  if (st.getAttribute("href") == "css/masterlight.css") {
-    st.href = "css/style.css";
-    th.textContent = "СВЕТЛАЯ ТЕМА";
+  if (st.getAttribute("href") == "master.css") {
+    st.href = "masterlight.css";
+    th.textContent = "ТЕМНАЯ ТЕМА";
   }
   else {
-    st.href = "css/masterlight.css";
-    th.textContent = "ТЕМНАЯ ТЕМА";
+    st.href = "master.css";
+    th.textContent = "СВЕТЛАЯ ТЕМА";
   };
   localStorage.removeItem('theme');
   localStorage.setItem('theme', st.href);
+  localStorage.removeItem('theme1');
+  localStorage.setItem('theme1', th.textContent);
 }
 st.href = localStorage.getItem('theme');
+th.textContent = localStorage.getItem('theme1');
