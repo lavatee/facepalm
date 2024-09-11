a538c31eeca5f5bbce0ea8da1f0cd04493a9e692
lavatee <161846965+lavatee@users.noreply.github.com>
2024-09-11 16:19:24 +0700

    Update tg.js

--- js/tg.js
+++ js/tg.js
@@ -1,18 +1,3 @@
-const tg = window.Telegram.WebApp;
-
-// Убедитесь, что веб-приложение инициализировано
-if (tg) {
-    // Получите информацию о пользователе
-    const userId = tg.initDataUnsafe.user.id; // ID пользователя
-    const username = tg.initDataUnsafe.user.username; // Username пользователя
-    const firstName = tg.initDataUnsafe.user.first_name; // Имя пользователя
-    const lastName = tg.initDataUnsafe.user.last_name; // Фамилия пользователя
-
-    console.log(`User ID: ${userId}`);
-    console.log(`Username: ${username}`);
-    console.log(`First Name: ${firstName}`);
-    console.log(`Last Name: ${lastName}`);
-}
 
 var mylist = document.getElementsByTagName("LI");
 var i;
