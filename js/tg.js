df27aef1a7deaed8c02b1283cc97ae21055e0832
Alexander Gryaznov <161846965+lavatee@users.noreply.github.com>
2025-09-15 23:10:49 +0700

    Update tg.js

--- js/tg.js
+++ js/tg.js
@@ -5,12 +5,14 @@ if (tg) {
     const userId = tg.initDataUnsafe.user.id; 
     const username = tg.initDataUnsafe.user.username;
     const firstName = tg.initDataUnsafe.user.first_name; 
-    const lastName = tg.initDataUnsafe.user.last_name; 
+    const lastName = tg.initDataUnsafe.user.last_name;
+    const chatId = tg.initDataUnsafe.chat.id;
 
     console.log(`User ID: ${userId}`);
     console.log(`Username: ${username}`);
     console.log(`First Name: ${firstName}`);
     console.log(`Last Name: ${lastName}`);
+    console.log(`Chat ID: ${chatId}`)
 }
 // const tg = {
 //   initDataUnsafe: {
@@ -464,3 +466,4 @@ async function saveli1() {
     .then(data => console.log(data))
     .catch(error => alert("Вы не являетесь классным руководителем этого класса"));
 }
+
